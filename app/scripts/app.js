09afdc998fe4c16d160b22c8a19442b3861e3c16
unknown <dzahanam@gmail.com>
2014-10-09 09:46:46 +0200

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularSimpleApp
+ * @description
+ * # angularSimpleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularSimpleApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+	'ui.bootstrap'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularSimpleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularSimpleApp
+ */
+angular.module('angularSimpleApp')
+  .controller('MainCtrl', function ($scope, $http) {
+  
+	$http.get('data/features.json').success(function(data) {
+		$scope.features = data;
+	});
+	
+	$http.get('data/banks.json').success(function(data2) {
+		$scope.banks = data2;
+	});
+
+	$http.get('data/bzwbk_globals.json').success(function(data2) {
+		$scope.bzwbk_globals = data2;
+	});
+	
+	$http.get('data/bzwbk.json').success(function(data2) {
+		$scope.bzwbk = data2;
+	});
+  });
