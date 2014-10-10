a4367dd171a9f32067bef29f21f9e0e8487914d2
unknown <dzahanam@gmail.com>
2014-10-10 15:02:34 +0200

    process init

--- app/scripts/controllers/process.js
+++ app/scripts/controllers/process.js
@@ -2,16 +2,26 @@
 
 /**
  * @ngdoc function
- * @name angularSimpleApp.controller:BankCtrl
+ * @name angularSimpleApp.controller:ProcessCtrl
  * @description
- * # BankCtrl
+ * # ProcessCtrl
  * Controller of the angularSimpleApp
  */
 angular.module('angularSimpleApp')
-  .controller('BankCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('ProcessCtrl', function ($scope) {
+
+  $scope.myInterval = 5000;
+  var slides = $scope.slides = [];
+  $scope.addSlide = function() {
+    var newWidth = 4 + slides.length;
+      image: 'images/data/bzwbk/indywidualny/sprzedaz_wewn/konto_walutowe/4.png',
+    slides.push({
+      text: ['Extra','Lots of','Extra','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<4; i++) {
+    $scope.addSlide();
+  }
+  
   });
